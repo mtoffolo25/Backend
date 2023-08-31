05783503942b55f430ec637101bc95354d13a614
Maxi <maxi_toffolo@hotmail.com>
2023-08-31 01:41:49 -0300

    Endpoints carrito terminado

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -1,19 +1,11 @@
 import { Router } from "express";
 import CartManager from "../services/db/cart.service.js";
 
-const router = Router();
+const cartRouter = Router();
 const manager = new CartManager();
 
-router.post('/', async (req, res) => {
-    try {
-        let status = await manager.addCart();
-        res.status(status.code).json({status: status.status});
-    } catch (error) {
-        res.status(500).json({ error: `Ocurrió un error en el servidor: ${error}` });
-    }
-})
 
-router.get('/:cid', async (req, res) => {
+cartRouter.get('/:cid', async (req, res) => {
     const { cid } = req.params;
     const cartProducts = await manager.getProductsOfCartById(cid);
     if(cartProducts) {
@@ -23,14 +15,65 @@ router.get('/:cid', async (req, res) => {
     }
 });
 
-router.post('/:cid/product/:pid', async (req, res) => {
+cartRouter.post('/', async (req, res) => {
     try {
-        const { cid, pid } = req.params;
-        let status = await manager.addProductToCart((cid), (pid));
+        let status = await manager.addCart();
         res.status(status.code).json({status: status.status});
     } catch (error) {
         res.status(500).json({ error: `Ocurrió un error en el servidor: ${error}` });
     }
 })
 
-export default router;
+cartRouter.post('/:cid/product/:pid', async (req, res) => {
+    try {
+        const { cid, pid } = req.params;
+        let status = await manager.addProductToCart(cid, pid);
+        res.status(status.code).json({status: status.status});
+    } catch (error) {
+        res.status(500).json({ error: `Ocurrió un error en el servidor: ${error}` });
+    }
+});
+
+cartRouter.put('/:cid', async (req, res) => {
+    try {
+        const { cid } = req.params;
+        const { products } = req.body;
+        const status = await manager.updateCart(cid, products);
+        res.status(status.code).json({ status: status.status });
+    } catch (error) {
+        res.status(500).json({ error: `Ocurrió un error en el servidor: ${error}` });
+    }
+});
+
+cartRouter.put('/:cid/product/:pid', async (req, res) => {
+    try {
+        const { cid, pid } = req.params;
+        const { quantity } = req.body;
+        const status = await manager.updateProductQuantity(cid, pid, quantity);
+        res.status(status.code).json({ status: status.status });
+    } catch (error) {
+        res.status(500).json({ error: `Ocurrió un error en el servidor: ${error}` });
+    }
+});
+
+cartRouter.delete('/:cid/product/:pid', async (req, res) => {
+    try {
+        const { cid, pid } = req.params;
+        const status = await manager.removeProductFromCart(cid, pid);
+        res.status(status.code).json({ status: status.status });
+    } catch (error) {
+        res.status(500).json({ error: `Ocurrió un error en el servidor: ${error}` });
+    }
+});
+
+cartRouter.delete('/:cid', async (req, res) => {
+    try {
+        const { cid } = req.params;
+        const status = await manager.removeAllProductsFromCart(cid);
+        res.status(status.code).json({ status: status.status });
+    } catch (error) {
+        res.status(500).json({ error: `Ocurrió un error en el servidor: ${error}` });
+    }
+});
+
+export default cartRouter;
